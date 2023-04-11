25eb63a039966e0fa0ac04c3c8dc7acf076b9498
kingcobraer <997818503@qq.com>
2023-04-11 12:10:07 +0800

    1

---
+++ test_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.write('HELLOooooooo, *World!* :sunglasses:')
+
+'### 这是三级标题'
+'>**hhh**'
+'*hhh*'
+'[点击跳转至百度](http://www.baidu.com)'
+'* AA'
+'* BB'
+'***'
+'---'
+'5. AAA'
+'5. BBB'
+
+st.sidebar.write('# 这是一级标题')
+st.sidebar.multiselect('这是多选框',["A","C","D","E"])
+
+
+
